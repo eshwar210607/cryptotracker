aff03c4d017cca486b735218b24818b5165803c5
eshwar210607 <eashwar210607@gmail.com>
2024-12-28 19:36:49 +0530

    homepage changes

--- homepage.js
+++ homepage.js
@@ -3,13 +3,12 @@ import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/fi
 import { getFirestore, getDoc, doc } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
 
 const firebaseConfig = {
-    // Your Firebase configuration goes here
-    apiKey: "your-api-key",
-    authDomain: "your-auth-domain",
-    projectId: "your-project-id",
-    storageBucket: "your-storage-bucket",
-    messagingSenderId: "your-messaging-sender-id",
-    appId: "your-app-id",
+    apiKey: "AIzaSyD9Z4GoIRONE5tspl7w0fvJTUEMIxPcL4Q",
+    authDomain: "cryptotracker-21.firebaseapp.com",
+    projectId: "cryptotracker-21",
+    storageBucket: "cryptotracker-21.firebasestorage.app",
+    messagingSenderId: "342349615474",
+    appId: "1:342349615474:web:ede4801d1f6e5cd4ec0deb"
 };
 
 // Initialize Firebase
@@ -18,35 +17,12 @@ const auth = getAuth();
 const db = getFirestore();
 
 document.addEventListener('DOMContentLoaded', () => {
-    // Check the authentication state of the user
-    onAuthStateChanged(auth, (user) => {
-        const loggedInUserId = localStorage.getItem('loggedInUserId');
-        if (loggedInUserId) {
-            const docRef = doc(db, "users", loggedInUserId);
-            getDoc(docRef)
-                .then((docSnap) => {
-                    if (docSnap.exists()) {
-                        const userData = docSnap.data();
-                        document.getElementById('loggedUserName').innerText = userData.userName;
-                        document.getElementById('loggedUserEmail').innerText = userData.email;
-                    } else {
-                        console.log("No document found matching ID");
-                    }
-                })
-                .catch((error) => {
-                    console.log("Error getting document:", error);
-                });
-        } else {
-            console.log("User ID not found in local storage");
-        }
-    });
-
     const logoutButton = document.getElementById('logout');
     logoutButton.addEventListener('click', () => {
-        localStorage.removeItem('loggedInUserId');
         signOut(auth)
             .then(() => {
-                window.location.href = 'http://127.0.0.1:5500/signup_page.html';
+                localStorage.removeItem('loggedInUserId');
+                window.location.href = 'http://127.0.0.1:5500/login_page.html';
             })
             .catch((error) => {
                 console.error('Error signing out:', error);
